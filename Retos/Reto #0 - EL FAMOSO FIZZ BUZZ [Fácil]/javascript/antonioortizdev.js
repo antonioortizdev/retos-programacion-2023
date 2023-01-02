2d7d002bfdad7a79db7c873c19197ff65cede952
Antonio Ortiz <antonioortizdev@gmail.com>
2023-01-02 13:00:53 +0100

    pass solution to main file

--- Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/javascript/antonioortizdev.js
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/javascript/antonioortizdev.js
@@ -1,15 +1,26 @@
-const showFizzBuzzOrNumber = (number) => {
-  const multiplesTextMap = {
-    3: 'fizz',
-    5: 'buzz',
-  }
-  
+FROM_NUMBER = 1
+TO_NUMBER = 100
+FIZZBUZZ_MAP = {
+  3: 'fizz',
+  5: 'buzz',
 }
 
-const showFizzBuzzBetweenTwoNumbers = (fromNumber, toNumber) => {
-  for (let currentNumber = fromNumber; currentNumber <= toNumber; ++currentNumber) {
-    showFizzBuzzOrNumber(currentNumber)
-  }
+const showFizzBuzzOrNumber = (multipleToTextMap, number) => {
+  const result = Object.keys(multipleToTextMap)
+    .reduce((text, multiple) => {
+      if (number % multiple === 0) {
+        return text + multipleToTextMap[multiple]
+      } 
+      return text
+    }, '')
+
+  return result || number
 }
 
-console.log(showFizzBuzzBetweenTwoNumbers(1, 100))
+const main = () => {
+  const numbers = Array.from({length: TO_NUMBER - FROM_NUMBER + 1}, (_, i) => i + FROM_NUMBER)
+  const log = numbers.map((number) => showFizzBuzzOrNumber(FIZZBUZZ_MAP, number)).join('\n')
+  console.log(log)
+}
+
+main()
