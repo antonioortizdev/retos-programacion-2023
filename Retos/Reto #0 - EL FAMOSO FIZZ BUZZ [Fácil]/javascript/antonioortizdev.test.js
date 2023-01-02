cceba3b7656ec9a9be2628ee9cbe7bf21722c63e
Antonio Ortiz <antonioortizdev@gmail.com>
2023-01-02 13:00:25 +0100

    refactor code

--- Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/javascript/antonioortizdev.test.js
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/javascript/antonioortizdev.test.js
@@ -1,17 +1,25 @@
-const showFizzBuzzOrNumber = (number) => {
-  if (number % 15 === 0) return 'fizzbuzz'
-  if (number % 5 === 0) return 'buzz'
-  if (number % 3 === 0) return 'fizz'
-  return number
+FROM_NUMBER = 1
+TO_NUMBER = 100
+FIZZBUZZ_MAP = {
+  3: 'fizz',
+  5: 'buzz',
+}
+
+const showFizzBuzzOrNumber = (multipleToTextMap, number) => {
+  const result = Object.keys(multipleToTextMap)
+    .reduce((text, multiple) => {
+      if (number % multiple === 0) {
+        return text + multipleToTextMap[multiple]
+      } 
+      return text
+    }, '')
+
+  return result || number
 }
 
 const main = () => {
-  const FROM_NUMBER = 1
-  const TO_NUMBER = 100
-  let log = ''
-  for (let currentNumber = FROM_NUMBER; currentNumber <= TO_NUMBER; ++currentNumber) {
-    log += `${showFizzBuzzOrNumber(currentNumber)}\n`
-  }
+  const numbers = Array.from({length: TO_NUMBER - FROM_NUMBER + 1}, (_, i) => i + FROM_NUMBER)
+  const log = numbers.map((number) => showFizzBuzzOrNumber(FIZZBUZZ_MAP, number)).join('\n')
   console.log(log)
 }
 
@@ -19,22 +27,22 @@ main()
 
 // TESTS
 test('Múltiplos de 3 por la palabra "fizz".', () => {
-  expect(showFizzBuzzOrNumber(3)).toBe('fizz')
-  expect(showFizzBuzzOrNumber(6)).toBe('fizz')
-  expect(showFizzBuzzOrNumber(9)).toBe('fizz')
+  expect(showFizzBuzzOrNumber(FIZZBUZZ_MAP, 3)).toBe('fizz')
+  expect(showFizzBuzzOrNumber(FIZZBUZZ_MAP, 6)).toBe('fizz')
+  expect(showFizzBuzzOrNumber(FIZZBUZZ_MAP, 9)).toBe('fizz')
 })
 test('Múltiplos de 5 por la palabra "buzz".', () => {
-  expect(showFizzBuzzOrNumber(5)).toBe('buzz')
-  expect(showFizzBuzzOrNumber(10)).toBe('buzz')
+  expect(showFizzBuzzOrNumber(FIZZBUZZ_MAP, 5)).toBe('buzz')
+  expect(showFizzBuzzOrNumber(FIZZBUZZ_MAP, 10)).toBe('buzz')
 })
 test('Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".', () => {
-  expect(showFizzBuzzOrNumber(15)).toBe('fizzbuzz')
-  expect(showFizzBuzzOrNumber(30)).toBe('fizzbuzz')
-  expect(showFizzBuzzOrNumber(45)).toBe('fizzbuzz')
+  expect(showFizzBuzzOrNumber(FIZZBUZZ_MAP, 15)).toBe('fizzbuzz')
+  expect(showFizzBuzzOrNumber(FIZZBUZZ_MAP, 30)).toBe('fizzbuzz')
+  expect(showFizzBuzzOrNumber(FIZZBUZZ_MAP, 45)).toBe('fizzbuzz')
 })
 test('that passing 1 prints 1', () => {
-  expect(showFizzBuzzOrNumber(1)).toBe(1)
+  expect(showFizzBuzzOrNumber(FIZZBUZZ_MAP, 1)).toBe(1)
 })
 test('that passing 100 prints "fizzbuzz"', () => {
-  expect(showFizzBuzzOrNumber(100)).toBe('buzz')
+  expect(showFizzBuzzOrNumber(FIZZBUZZ_MAP, 100)).toBe('buzz')
 })
